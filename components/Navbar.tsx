999d5882444a71212cd6a4f89cca64bc4aa120cf
Marina Esac <marina.esac@coformatique.com>
2025-06-04 02:50:29 +0300

    Implemented Auth, Billing, Database Setup, Create Companion Server action

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import NavItems from "./NavItems";
 
 const Navbar = () => {
@@ -7,17 +8,19 @@ const Navbar = () => {
     <nav className="navbar">
       <Link href="/">
         <div className="flex items-center gap-2.5 cursor-pointer">
-            <Image
-                src="/images/logo.svg"
-                alt="Logo"
-                width={46}
-                height={44}
-            />
+          <Image src="/images/logo.svg" alt="Logo" width={46} height={44} />
         </div>
       </Link>
       <div className="flex items-center gap-8">
-         <NavItems />
-         <p>Sign In</p>
+        <NavItems />
+        <SignedOut>
+          <SignInButton>
+            <button className="btn-signin">Sign In</button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
     </nav>
   );
